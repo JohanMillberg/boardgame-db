dcfc8a8ad1ba61dc6e4b95c2e11ec6294724eb01
JohanMillberg <johanmillberg@me.com>
2024-08-31 22:38:07 +0200

    fixed authentication

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,14 +1,7 @@
 import prisma from '../db';
 import { Request, Response } from 'express';
 
-interface CreateUserRequest extends Request {
-    body: {
-        username: string;
-        password: string;
-    }
-}
-
-export const createNewUser = async (req: CreateUserRequest, res: Response) => {
+export const createNewUser = async (req: Request, res: Response) => {
     const user = await prisma.user.create({
         data: {
             username: req.body.username,
@@ -16,5 +9,5 @@ export const createNewUser = async (req: CreateUserRequest, res: Response) => {
         }
     })
 
-    res.json()
+    res.json({ data: user })
 }
